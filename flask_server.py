0a8a8f44ea86899369668441cbd2820a4ff3ea86
Sharanya Ranka <sharanya.ranka@gmail.com>
2020-07-04 19:29:27 +0530

    Changes done flask_server.py made to run a flask server for training and predicting some template html files(training_form and form_received) a python file to make json config files

---
+++ flask_server.py
@@ -0,0 +1,41 @@
+from flask import Flask,render_template,request
+
+app = Flask(__name__)
+
+
+
+@app.route('/training', methods=['GET','POST'])
+def get_training_data():
+    if (request.method=='GET'):
+        return render_template("training_form.html")
+
+    elif (request.method == 'POST'):
+        form=request.form
+        print(form['config_file'])
+        return render_template("form_received.html",form=form)
+
+
+# @app.route('/', methods=['GET'])
+# def hello():
+#     return "Hello"
+#
+#
+# if __name__ == '__main__':
+#     json_config = get_config('config.json')
+#
+#     upload_path = json_config['default_parameters']['upload_directory']
+#     detected_path = json_config['default_parameters']['detected_directory']
+#     # checking if directories for uploaded and detected images exist if not create them
+#     check_dirs([upload_path, detected_path])
+#
+#     # loading the model before start of server
+#     t0 = time.perf_counter()
+#     detector = load_model(json_config['model_file']['default'], json_config['default_parameters']['default_speed'])
+#     t1 = time.perf_counter()
+#
+#     print("Time elapsed in loading model:", t1 - t0)
+#
+#     app.run(debug=True, host=json_config['host'], port=json_config['port'])
+
+if __name__=='__main__':
+    app.run(debug=True)
---
+++ make_config_file.py
@@ -0,0 +1,8 @@
+import json
+
+data={}
+data["training data path"]=""
+#data["hyperparameters"]={}
+data["log file path"]=""
+data["testing data path"]=""
+data["save model path"]=""
